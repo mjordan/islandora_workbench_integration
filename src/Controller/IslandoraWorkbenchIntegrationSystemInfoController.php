686ede7030a50a3323c1c65a4cc14bd640f01afc
Mark Jordan <mjordan@sfu.ca>
2022-09-13 14:12:24 +0000

    Added default_socket_timeout to output of /islandora_workbench_integration/system_info as per https://github.com/mjordan/islandora_workbench/issues/353.

--- src/Controller/IslandoraWorkbenchIntegrationSystemInfoController.php
+++ src/Controller/IslandoraWorkbenchIntegrationSystemInfoController.php
@@ -17,11 +17,13 @@ class IslandoraWorkbenchIntegrationSystemInfoController extends ControllerBase {
      $post_max_size = ini_get('post_max_size');
      $max_execution_time = ini_get('max_execution_time');
      $upload_max_filesize = ini_get('upload_max_filesize');
+     $default_socket_timeout = ini_get('default_socket_timeout');
      return new JsonResponse([
        'memory_limit' => $memory_limit,
        'post_max_size' => $post_max_size,
-       'max_execution_time' => $max_execution_time,
        'upload_max_filesize' => $upload_max_filesize,
+       'max_execution_time' => $max_execution_time,
+       'default_socket_timeout' => $default_socket_timeout,
      ]);
    }
 
